51cb3df0721f31937693ecf0d99d88b84b43fe01
NuclearW <incongruency@gmail.com>
2013-01-14 16:59:50 -0500

    Convict command

--- src/main/java/com/nuclearw/rapsheet/commands/ConvictCommandExecutor.java
+++ src/main/java/com/nuclearw/rapsheet/commands/ConvictCommandExecutor.java
@@ -1,10 +1,13 @@
 package com.nuclearw.rapsheet.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.nuclearw.rapsheet.Rapsheet;
 import com.nuclearw.rapsheet.Record;
+import com.nuclearw.rapsheet.RecordState;
 
 public class ConvictCommandExecutor extends BaseCommandExecutor {
 	public ConvictCommandExecutor(Rapsheet plugin) {
@@ -35,6 +38,36 @@ public class ConvictCommandExecutor extends BaseCommandExecutor {
 		if(found == null) {
 			sender.sendMessage(COULD_NOT_FIND_CHARGE.replace("<PLAYER>", target));
 			return true;
+		}
+
+		if(found.getState() != RecordState.CHARGED) {
+			String message = ChatColor.RED + "You cannot convict for a charge that ";
+
+			switch(found.getState()) {
+				case CONVICTED:
+					message += "they are already convicted for!";
+					break;
+				case PARDONED:
+					message += "have been pardoned for!";
+					break;
+			}
+			return true;
+		}
+
+		found.setState(RecordState.CONVICTED);
+
+		plugin.getDatabase().update(found);
+
+		sender.sendMessage(ChatColor.DARK_RED + "Convicted" + ChatColor.RESET + ": " + ChatColor.AQUA + found.getOffender());
+		sender.sendMessage(ChatColor.GOLD + "Charge " + ChatColor.RESET + "#" + found.getChargeId() + ChatColor.GOLD + " - " + ChatColor.AQUA + found.getChargeShort());
+		sender.sendMessage(ChatColor.GOLD + "Report" + ChatColor.RESET + ": " + ChatColor.GRAY + found.getChargeLong());
+
+		// Notify player if they are online
+		Player player = plugin.getServer().getPlayer(target);
+
+		if(player != null) {
+			player.sendMessage(ChatColor.DARK_RED + "Convicted" + ChatColor.GOLD + " by " + ChatColor.AQUA + sender.getName() + ChatColor.GOLD + " of " + ChatColor.GRAY + found.getChargeShort());
+			player.sendMessage(ChatColor.GOLD + "Filed under Charge " + ChatColor.RESET + "#" + found.getChargeId());
 		}
 
 		return true;
