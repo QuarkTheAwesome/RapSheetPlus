56a138835b313d8c394fdba6faa199e6db35972e
NuclearW <incongruency@gmail.com>
2013-01-14 18:03:48 -0500

    Expunge command

--- src/main/java/com/nuclearw/rapsheet/commands/ExpungeCommandExecutor.java
+++ src/main/java/com/nuclearw/rapsheet/commands/ExpungeCommandExecutor.java
@@ -1,7 +1,9 @@
 package com.nuclearw.rapsheet.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.nuclearw.rapsheet.Rapsheet;
 import com.nuclearw.rapsheet.Record;
@@ -36,6 +38,20 @@ public class ExpungeCommandExecutor extends BaseCommandExecutor {
 			sender.sendMessage(COULD_NOT_FIND_CHARGE.replace("<PLAYER>", target));
 			return true;
 		}
+
+		plugin.getDatabase().delete(found);
+
+		sender.sendMessage(ChatColor.GRAY + "Expunged " + ChatColor.GOLD + "charge " + ChatColor.RESET + "#" + found.getChargeId() + ChatColor.GOLD + " - " + ChatColor.AQUA + found.getChargeShort());
+
+		// Notify player if they are online
+		Player player = plugin.getServer().getPlayer(target);
+
+		if(player == null) {
+			return true;
+		}
+
+		player.sendMessage(ChatColor.GOLD + "Charge " + ChatColor.RESET + "#" + found.getChargeId() + ChatColor.GOLD + " has been " + ChatColor.GRAY + "expunged" + ChatColor.GOLD + " by " + ChatColor.AQUA + sender.getName());
+
 
 		return true;
 	}
