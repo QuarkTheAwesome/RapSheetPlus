04a779c9709c3119848176b9c819855806dcccee
NuclearW <incongruency@gmail.com>
2013-01-14 13:38:53 -0500

    Begin charge command

--- src/main/java/com/nuclearw/rapsheet/commands/ChargeCommandExecutor.java
+++ src/main/java/com/nuclearw/rapsheet/commands/ChargeCommandExecutor.java
@@ -1,9 +1,14 @@
 package com.nuclearw.rapsheet.commands;
 
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.nuclearw.rapsheet.Rapsheet;
+import com.nuclearw.rapsheet.Record;
+import com.nuclearw.rapsheet.RecordState;
 
 public class ChargeCommandExecutor extends BaseCommandExecutor {
 	public ChargeCommandExecutor(Rapsheet plugin) {
@@ -17,6 +22,41 @@ public class ChargeCommandExecutor extends BaseCommandExecutor {
 			printHelp(sender, label);
 			return true;
 		}
+
+		// We won't use findTarget because we're not going to allow charging of offline players.
+		Player offender = plugin.getServer().getPlayer(args[1]);
+
+		if(offender == null) {
+			sender.sendMessage(COULD_NOT_FIND_PLAYER);
+			return true;
+		}
+
+		int newChargeId = 1;
+
+		// I do not like the way this is done
+		List<Record> found = plugin.getDatabase().find(Record.class).where().ieq("offender", offender.getName()).findList();
+
+		if(found != null && !found.isEmpty()) {
+			newChargeId = found.get(found.size() - 1).getChargeId() + 1;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		// TODO: Make charge description from args
+
+		String chargeDescription = sb.toString().trim();
+
+		Record record = new Record();
+		record.setChargeId(newChargeId);
+		record.setOffender(offender.getName());
+		record.setOfficial(sender.getName());
+		record.setChargeLong(args[2]);
+		record.setChargeLong(chargeDescription);
+		record.setTime(System.currentTimeMillis());
+		record.setSealed(false);
+		record.setState(RecordState.CHARGED);
+
+		plugin.getDatabase().save(record);
 
 		return true;
 	}
