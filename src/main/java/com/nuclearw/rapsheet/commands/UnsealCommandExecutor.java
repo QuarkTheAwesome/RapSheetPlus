c9f4730a8fae743f0b0eaa9b210669e759ed9004
NuclearW <incongruency@gmail.com>
2013-01-14 17:58:44 -0500

    Unseal command

--- src/main/java/com/nuclearw/rapsheet/commands/UnsealCommandExecutor.java
+++ src/main/java/com/nuclearw/rapsheet/commands/UnsealCommandExecutor.java
@@ -1,7 +1,9 @@
 package com.nuclearw.rapsheet.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.nuclearw.rapsheet.Rapsheet;
 import com.nuclearw.rapsheet.Record;
@@ -36,6 +38,26 @@ public class UnsealCommandExecutor extends BaseCommandExecutor {
 			sender.sendMessage(COULD_NOT_FIND_CHARGE.replace("<PLAYER>", target));
 			return true;
 		}
+
+		if(!found.isSealed()) {
+			sender.sendMessage(ChatColor.RED + "You cannot unseal a charge that isn't sealed!");
+			return true;
+		}
+
+		found.setSealed(true);
+
+		plugin.getDatabase().update(found);
+
+		sender.sendMessage(ChatColor.GRAY + "Unsealed " + ChatColor.GOLD + "charge " + ChatColor.RESET + "#" + found.getChargeId() + ChatColor.GOLD + " - " + ChatColor.AQUA + found.getChargeShort());
+
+		// Notify player if they are online
+		Player player = plugin.getServer().getPlayer(target);
+
+		if(player == null) {
+			return true;
+		}
+
+		player.sendMessage(ChatColor.GOLD + "Charge " + ChatColor.RESET + "#" + found.getChargeId() + ChatColor.GOLD + " has been " + ChatColor.GRAY + "unsealed" + ChatColor.GOLD + " by " + ChatColor.AQUA + sender.getName());
 
 		return true;
 	}
